9a4a8b1ade7498ce259b5935162cf91797e300c3
Anvi Penmetsa <anvipenmetsa@gmail.com>
2023-04-22 10:55:08 -0700

    Setup Firebase

---
+++ firebase.js
@@ -0,0 +1,21 @@
+require('dotenv').config()
+// Import the functions you need from the SDKs you need
+import "firebase/database";
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: process.env.FIREBASE_API,
+  authDomain: process.env.FIREBASE_AUTHDOMAIN,
+  projectId: process.env.FIREBASE_PROJECTAPI,
+  storageBucket: FIREBASE_STORAGEBUCKET,
+  messagingSenderId: FIREBASE_MESSAGINGSENDERID,
+  appId: FIREBASE_APPID
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const database = getFirestore(app);
