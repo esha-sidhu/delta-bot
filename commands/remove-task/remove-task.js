6cde5b1d48dc3f00437472d3af0944ac308daf3c
ekuczynski <109834726+ekuczynski@users.noreply.github.com>
2023-04-23 01:47:31 -0700

    Finish remove item from list

--- commands/remove-task/remove-task.js
+++ commands/remove-task/remove-task.js
@@ -1,22 +1,76 @@
 const { SlashCommandBuilder } = require('discord.js');
+const {addDoc, collection, getDocs, query, where, updateDoc, doc} = require('firebase/firestore');
+const {database} = require('../../firebase');
 
 module.exports = {
 	data: new SlashCommandBuilder()
 	    .setName('remove')
 	    .setDescription('Removes/deletes a task from the task list')
 
-	    .addStringOption(option =>
-		    option.setName('input-task')
-			    .setDescription('The task to remove')
-                .setRequired(true))
-
-        .addBooleanOption(option =>
-            option.setName('ephemeral')
-                .setDescription('Whether or not the remove should be ephemeral')),
+        .addIntegerOption(option =>
+            option.setName('tasknumber')
+            .setDescription('Number of Task to Remove (starting with 1)')
+            .setRequired(true)
+        ),
     
     async execute(interaction) {
-        const task = interaction.options.getString('input-task');
-        await interaction.reply(`Task removed: ${task}`);
-        console.log(`Task removed: ${task}`);
+        const user = interaction.user.username;
+        const userID = interaction.user.id;
+        const task_index = interaction.options.getInteger('tasknumber') - 1;
+        const listQ = query(collection(database, "taskListData"), where("userID", "==", userID));
+        const listQRes = await getDocs(listQ);
+
+        if (listQRes.docs.length !== 0)
+        {
+            console.log("user found");
+            const ID = listQRes.docs[0].id;
+            const docToUpdate = doc(database, "taskListData", ID);
+            let tempTask = listQRes.docs[0]._document.data.value.mapValue.fields.task.arrayValue.values;
+            let tempDeadline = listQRes.docs[0]._document.data.value.mapValue.fields.deadline.arrayValue.values;
+            let tempHour = listQRes.docs[0]._document.data.value.mapValue.fields.reminderHour.arrayValue.values;
+            let tempMin = listQRes.docs[0]._document.data.value.mapValue.fields.reminderMinute.arrayValue.values;
+
+            let taskList = [];
+            let lenT = tempTask.length;
+
+            let task = "";
+            for (let i = 0; i < lenT; i++)
+            {
+                if (i != task_index) {
+                    taskList.push(tempTask[i].stringValue);
+                }
+                else{
+                    task = tempTask[i].stringValue;
+                }
+            }
+
+            let deadlineList = [];
+            for (let i = 0; i < lenT; i++)
+            {
+                if (i != task_index) {
+                    deadlineList.push(tempDeadline[i].stringValue);
+                }
+            }
+
+            let hourList = [];
+            for (let i = 0; i < lenT; i++)
+            {
+                if (i != task_index) {
+                    hourList.push(tempHour[i].stringValue);
+                }
+            }
+
+            let minList = [];
+            for (let i = 0; i < lenT; i++)
+            {
+                if (i != task_index) {
+                    minList.push(tempMin[i].stringValue);
+                }
+            }
+
+            // console.log(docToUpdate);
+            await updateDoc(docToUpdate, {task: taskList, deadline: deadlineList, reminderHour: hourList, reminderMinute: minList});
+            await interaction.reply(`Task removed: ${task}`);
+        }
     },
 };
