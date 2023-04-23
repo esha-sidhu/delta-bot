b1748414bbd551f5e9f99cbd95372725814a6a23
Snigdha Kansal <snigdha0206@g.ucla.edu>
2023-04-23 00:49:49 -0700

    implements remind command

--- commands/reminders/reminders.js
+++ commands/reminders/reminders.js
@@ -1,5 +1,7 @@
 const { SlashCommandBuilder } = require('discord.js');
 
+const sleep = m => new Promise(r => setTimeout(r, m));
+
 module.exports = {
 	data: new SlashCommandBuilder()
 	    .setName('remind')
@@ -12,7 +14,7 @@ module.exports = {
 
         .addStringOption(option =>
             option.setName('input-reminder-period')
-                .setDescription('The frequency of time to remind the user of the task (in hours)')
+                .setDescription('The frequency of time to remind the user of the task (in seconds)')
                 .setRequired(true))
 
         .addBooleanOption(option =>
@@ -21,8 +23,20 @@ module.exports = {
     
     async execute(interaction) {
         const task = interaction.options.getString('input-task');
-        const remPeriod = interaction.options.getString('input-reminder-period');
-        await interaction.reply(`${remPeriod} hour reminder added for task ${task}`);
-        console.log(`${remPeriod} hour reminder added for task ${task}`);
+        let remPeriod = interaction.options.getString('input-reminder-period');
+        const deadline = 100000;
+        await interaction.reply(`${remPeriod} seconds reminder added for task ${task}`);
+        remPeriod*=1000;
+        var timer = 0;
+
+        while(timer < deadline) {
+            await sleep(1000);
+            timer += 1000;
+            
+            if(timer % remPeriod == 0)
+            {
+                await interaction.followUp('reminder!');
+            }
+        }
     },
 };
